6b8c713b782d329195cebdb16fc7c8b203e77674
rachel-gamble <sautrah@gmail.com>
2023-03-16 17:53:19 -0600

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Vue ToDo</h1>
+      <input class="input" v-model="newToDo" />
+      <button v-on:click="add()" class="add-button bold">+</button>
+      <ul class="list" style="list-style-type: none">
+        <li v-for="(todo, i) in exisitingToDo">
+          <span>
+            {{ i + 1 }}: {{ todo.text }}
+          </span>
+          <button @click="deleteToDo" class="delete-button">
+            X
+          </button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      newToDo: '',
+      exisitingToDo: [
+        { text: 'New Coding Project', id: 0 },
+        { text: 'Go to ballet', id: 1 },
+        { text: 'Date Night', id: 2 },
+      ]
+    }
+  },
+  methods: {
+    add() {
+      this.exisitingToDo.push({
+        text: this.newToDo,
+        id: new Date().valueOf()
+      }),
+        this.newToDo = ''
+    },
+    deleteToDo(i) {
+      this.exisitingToDo.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style></style>
